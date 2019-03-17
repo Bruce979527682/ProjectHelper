30a2a9d84028972e6b4951fb78122513d0829798
Bruce <979527682@qq.com>
2019-03-17 20:55:58 +0800

    2019-03-17-4

--- cshelp.cs
+++ cshelp.cs
@@ -71,7 +71,7 @@ namespace ConsoleTest
                                 var address = address1.children.Find(x => x.value.Equals(strs2[0]));
                                 if (address != null)
                                 {
-                                    address.children.Add(new address { value = strs3[0], label = strs3[1], children = new List<address>() });
+                                    address.children.Add(new address { value = strs3[0], label = strs3[1] });
                                 }
                             }
                         }
@@ -86,16 +86,84 @@ namespace ConsoleTest
                             var address = addresslist.Find(x => x.value.Equals(strs1[0]));
                             if (address != null)
                             {
-                                address.children.Add(new address { value = strs3[0], label = strs3[1], children = new List<address>() });
+                                address.children.Add(new address { value = strs3[0], label = strs3[1] });
                             }
                         }
                     }
                 }
 
-                var listt = addresslist;
+                sb.AppendLine("");
+                sb.AppendLine("[");
+                foreach (var items in addresslist)
+                {
+                    sb.AppendLine("{");
+                    sb.AppendLine("\"label\":\"" + items.label.Trim() + "\",");
+                    if (items.children != null && items.children.Count > 0)
+                    {
+                        sb.AppendLine("\"value\":\"" + items.value.Trim() + "\",");
+                    }
+                    else
+                    {
+                        sb.AppendLine("\"value\":\"" + items.value.Trim() + "\"");
+                    }                    
+                    if (items.children != null && items.children.Count > 0)
+                    {
+                        sb.AppendLine("\"children\":[");
+                        foreach (var item in items.children)
+                        {
+                            sb.AppendLine("{\"label\":\"" + item.label.Trim() + "\",");
+                            if (item.children != null && item.children.Count > 0)
+                            {
+                                sb.AppendLine("\"value\":\"" + item.value.Trim() + "\",");
+                            }
+                            else
+                            {
+                                sb.AppendLine("\"value\":\"" + item.value.Trim() + "\"");
+                            }                                
+                            if (item.children != null && item.children.Count > 0)
+                            {
+                                sb.AppendLine("\"children\":[");
+                                foreach (var it in item.children)
+                                {
+                                    sb.AppendLine("{\"label\":\"" + it.label.Trim() + "\",");
+                                    if (item.children.IndexOf(it) == item.children.Count - 1)
+                                    {
+                                        sb.AppendLine("\"value\":\"" + it.value.Trim() + "\"}");
+                                    }
+                                    else
+                                    {
+                                        sb.AppendLine("\"value\":\"" + it.value.Trim() + "\"},");
+                                    }
+
+                                }
+                                sb.AppendLine("]");
+                            }
+                            
+                            if (items.children.IndexOf(item) == items.children.Count - 1)
+                            {
+                                sb.AppendLine("}");
+                            }
+                            else
+                            {
+                                sb.AppendLine("},");
+                            }
+                        }
+                        sb.AppendLine("]");
+                    }               
+                    
+                    if (addresslist.IndexOf(items) == addresslist.Count - 1)
+                    {
+                        sb.AppendLine("}");
+                    }
+                    else
+                    {
+                        sb.AppendLine("},");
+                    }
+                }
+                sb.AppendLine("]");
+                sb.AppendLine("");
+                var html = sb.ToString();
             }
-
-
         }
 
         public class address
