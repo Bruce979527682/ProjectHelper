150bb4a8144003ce5b75dce9a6be5e39d2a64b9f
Bruce <979527682@qq.com>
2019-03-24 14:44:22 +0800

    2019-03-24-2

--- readcode.py
+++ readcode.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-#readfile = open(r'C:\work\Friend-0817\User.MiniSNS\Views\Friend\advertlist.cshtml')
-readfile2 = open('C:/work/Friend-0817/User.MiniSNS/Views/Friend/advertlist.cshtml','r',encoding="utf-8")
-try:
-     fileContent = readfile2.readlines()
-#     fileContent = readfile.read()
-#     fileContent = readfile.readline()
+import datetime
+import os
+
+cdate = datetime.datetime.now().strftime('%Y-%m-%d')
+filepath = 'C:/Users/EDZ/Desktop/'+cdate
+isExists=os.path.exists(filepath)
+if not isExists:
+    os.makedirs(filepath)
+
+readfile = open(r'C:\work\Friend-0817\User.MiniSNS\Views\Friend\activitylist.cshtml','r', encoding='UTF-8')     
+lines =readfile.readlines()
+clist=[]
+for line in lines:
+     line = line.lstrip('\ufeff')
+     line = line.rstrip('\n')
+     line = line.replace('\'','"')
+     clist.append('    sb.append(\''+ line +'\'+\'\\n\')'+'\n')
+
+fout = open(filepath + '/newfile.txt', "w", encoding='utf-8')
+fout.writelines(clist)
+fout.close()
+print('done')
