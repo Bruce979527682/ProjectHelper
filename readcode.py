1b3b9e17c177ecb6d1768f840b24b5adffebc476
BruceChen <979527682@qq.com>
2019-05-28 11:19:26 +0800

    2

--- readcode.py
+++ readcode.py
@@ -3,8 +3,11 @@
 import datetime
 import os
 
+def GetDesktopPath():
+    return os.path.join(os.path.expanduser("~"), 'Desktop')
+
 cdate = datetime.datetime.now().strftime('%Y-%m-%d')
-filepath = 'C:/Users/EDZ/Desktop/'+cdate
+filepath = GetDesktopPath() + '\\'+ cdate
 isExists=os.path.exists(filepath)
 if not isExists:
     os.makedirs(filepath)
@@ -18,7 +21,7 @@ for line in lines:
      line = line.replace('\'','"')
      clist.append('    sb.append(\''+ line +'\'+\'\\n\')'+'\n')
 
-fout = open(filepath + '/newfile.txt', "w", encoding='utf-8')
+fout = open(filepath + '\\newfile.txt', "w", encoding='utf-8')
 fout.writelines(clist)
 fout.close()
 print('done')
